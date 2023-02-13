3c2d63036e1e6d5c2b1b3c76c122136ad821f084
Blake Reimer <sobeksea@gmail.com>
2023-02-13 21:25:36 +0000

    update npm version for expo

--- App.tsx
+++ App.tsx
@@ -59,7 +59,7 @@ const App = () => {
           component={FooterMenu}
           options={{ gestureEnabled: false }}
         />
-        <Stack.Screen name="TopCategories" component={Screens.Offline} />
+        <Stack.Screen name="TopCategories" component={Screens.TopCategories} />
         <Stack.Screen name="Explore" component={FooterMenu} />
         <Stack.Screen name="My Learning" component={FooterMenu} />
         <Stack.Screen name="Account" component={FooterMenu} />
