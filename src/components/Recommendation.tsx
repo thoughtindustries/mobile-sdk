b1afdc919701513a52c32a2eb02bcf1ad570621b
Blake Reimer <sobeksea@gmail.com>
2023-06-03 01:53:11 +0000

    render data for new user

--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -11,55 +11,104 @@ import {
   TouchableOpacity,
 } from "react-native";
 import _ from "lodash";
-import { useUserContentItemsQuery } from "../graphql";
+import { useUserContentItemsQuery, useCatalogContentQuery } from "../graphql";
+import { GlobalTypes } from "../graphql";
 
 type HomeScreenProps = StackNavigationProp<RootStackParamList, "Home">;
 
 const Recommendation = () => {
-  const { data } = useUserContentItemsQuery();
+  const { data: contentData } = useUserContentItemsQuery();
+  const { data: catalogData } = useCatalogContentQuery({
+    variables: {
+      sortColumn: GlobalTypes.SortColumn.Title,
+      sortDirection: GlobalTypes.SortDirection.Asc,
+      page: 1,
+      labels: [],
+      values: [],
+      contentTypes: "Course",
+    },
+  });
   const navigation = useNavigation<HomeScreenProps>();
+
+  const CourseList = () => (
+    <ScrollView
+      horizontal={true}
+      style={styles.courseContainer}
+      showsHorizontalScrollIndicator={false}
+    >
+      {catalogData?.CatalogContent?.contentItems?.map((course, idx) => (
+        <TouchableOpacity
+          key={idx}
+          onPress={() =>
+            navigation.navigate("ContentDetails", {
+              cid: course.id,
+              from: "Home",
+            })
+          }
+        >
+          <View style={styles.recContentBox}>
+            <ImageBackground
+              key={idx}
+              source={{ uri: course.asset }}
+              resizeMode="cover"
+              style={{ borderRadius: 10 }}
+              imageStyle={{ borderRadius: 8 }}
+            >
+              <View style={styles.bannerArea}>
+                <Text style={styles.courseTitle}>{course.title}</Text>
+              </View>
+            </ImageBackground>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
+
+  const ContentList = () => (
+    <ScrollView
+      horizontal={true}
+      style={styles.courseContainer}
+      showsHorizontalScrollIndicator={false}
+    >
+      {contentData?.UserContentItems?.filter(
+        (item) => item.contentTypeLabel === "Course"
+      ).map((course, idx) => (
+        <TouchableOpacity
+          key={idx}
+          onPress={() =>
+            navigation.navigate("ContentDetails", {
+              cid: course.displayCourse ? course.displayCourse : "",
+              from: "Home",
+            })
+          }
+        >
+          <View style={styles.recContentBox}>
+            <ImageBackground
+              key={idx}
+              source={{ uri: course.asset }}
+              resizeMode="cover"
+              style={{ borderRadius: 10 }}
+              imageStyle={{ borderRadius: 8 }}
+            >
+              <View style={styles.bannerArea}>
+                <Text style={styles.courseTitle}>{course.title}</Text>
+              </View>
+            </ImageBackground>
+          </View>
+        </TouchableOpacity>
+      ))}
+    </ScrollView>
+  );
 
   return (
     <View>
-      {data && data.UserContentItems && (
-        <>
-          <View style={styles.courseBox}>
-            <Text style={styles.heading}>Recommendations</Text>
-          </View>
-          <ScrollView
-            horizontal={true}
-            style={styles.courseContainer}
-            showsHorizontalScrollIndicator={false}
-          >
-            {data.UserContentItems.filter(
-              (item) => item.contentTypeLabel === "Course"
-            ).map((course, idx) => (
-              <TouchableOpacity
-                key={idx}
-                onPress={() =>
-                  navigation.navigate("ContentDetails", {
-                    cid: course.displayCourse ? course.displayCourse : "",
-                    from: "Home",
-                  })
-                }
-              >
-                <View style={styles.recContentBox}>
-                  <ImageBackground
-                    key={idx}
-                    source={{ uri: course.asset }}
-                    resizeMode="cover"
-                    style={{ borderRadius: 10 }}
-                    imageStyle={{ borderRadius: 8 }}
-                  >
-                    <View style={styles.bannerArea}>
-                      <Text style={styles.courseTitle}>{course.title}</Text>
-                    </View>
-                  </ImageBackground>
-                </View>
-              </TouchableOpacity>
-            ))}
-          </ScrollView>
-        </>
+      <View style={styles.courseBox}>
+        <Text style={styles.heading}>Recommendations</Text>
+      </View>
+      {contentData?.UserContentItems?.length !== 0 ? (
+        <ContentList />
+      ) : (
+        <CourseList />
       )}
     </View>
   );
